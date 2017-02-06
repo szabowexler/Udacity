21904eafe16b1347741373caf867fe93bbfab8fe
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2017-02-06 13:57:26 -0500

    Add naive bayes classifier impl

--- ud120-intro-to-machine-learning/naive-bayes/ClassifyNB.py
+++ ud120-intro-to-machine-learning/naive-bayes/ClassifyNB.py
@@ -1,9 +1,5 @@
 def classify(features_train, labels_train):
-### import the sklearn module for GaussianNB
-### create classifier
-### fit the classifier on the training features and labels
-### return the fit classifier
-
-
-### your code goes here!
-
+    from sklearn.naive_bayes import GaussianNB
+    classifier = GaussianNB()
+    classifier.fit(features_train, labels_train)
+    return classifier

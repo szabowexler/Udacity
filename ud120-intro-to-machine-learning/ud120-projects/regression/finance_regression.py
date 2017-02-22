f8261adc3396588cdb5c9b6a7a8b0cdc5cc0c81a
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2017-02-22 14:29:15 -0500

    Linear regression work

--- ud120-intro-to-machine-learning/ud120-projects/regression/finance_regression.py
+++ ud120-intro-to-machine-learning/ud120-projects/regression/finance_regression.py
@@ -22,28 +22,27 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "m=", reg.coef_
+print "b=", reg.intercept_
+print "Training score=", reg.score(feature_train, target_train)
+print "Testing score=", reg.score(feature_test, target_test)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -56,14 +55,19 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+print "m=", reg.coef_
+print "b=", reg.intercept_
+print "Training score=", reg.score(feature_train, target_train)
+print "Testing score=", reg.score(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
